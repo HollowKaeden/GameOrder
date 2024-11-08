6d1383229797c7a990b0759858d2761aabd82f17
Ivan Artamonov <Vanya200373@yandex.ru>
2024-11-08 13:35:01 +0300

    Configure project settings

--- GameOrder/GameOrder/settings.py
+++ GameOrder/GameOrder/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin_panel.apps.AdminPanelConfig',
+    'order_page.apps.OrderPageConfig',
+    'registration.apps.RegistrationConfig',
 ]
 
 MIDDLEWARE = [
@@ -47,10 +50,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'GameOrder.urls'
 
+TEMPLATES_DIR = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -101,6 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
