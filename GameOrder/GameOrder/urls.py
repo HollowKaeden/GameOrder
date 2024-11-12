8ec4e669f3560fc57c931bcb235f6a270162c059
Ivan Artamonov <Vanya200373@yandex.ru>
2024-11-12 07:55:20 +0300

    Add index page

--- GameOrder/GameOrder/urls.py
+++ GameOrder/GameOrder/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', include('admin_panel.urls', namespace='admin_panel')),
     path('order/', include('order_page.urls', namespace='order_page')),
-    path('', include('registration.urls', namespace='registration'))
+    path('registration/', include('registration.urls',
+                                  namespace='registration')),
+    path('', include('main.urls', namespace='main')),
 ]
